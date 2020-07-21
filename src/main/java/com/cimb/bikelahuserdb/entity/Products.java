f68a46507e1a7b8ba155ed94c73e7e470af12e70
Daniel Roberto <32633091+danielroberto8@users.noreply.github.com>
2020-07-21 07:20:35 +0700

    ganti nama filed 'desc' ke 'description' biar ga error

--- src/main/java/com/cimb/bikelahuserdb/entity/Products.java
+++ src/main/java/com/cimb/bikelahuserdb/entity/Products.java
@@ -17,77 +17,59 @@ public class Products {
 	private int discount;
 	private String category;
 	private String image;
-	private String desc;
+	private String description;
 	private int totalPurchased;
-
 	public int getId() {
 		return id;
 	}
-
 	public void setId(int id) {
 		this.id = id;
 	}
-
 	public String getProductName() {
 		return productName;
 	}
-
 	public void setProductName(String productName) {
 		this.productName = productName;
 	}
-
 	public int getPrice() {
 		return price;
 	}
-
 	public void setPrice(int price) {
 		this.price = price;
 	}
-
 	public int getQuantity() {
 		return quantity;
 	}
-
 	public void setQuantity(int quantity) {
 		this.quantity = quantity;
 	}
-
 	public int getDiscount() {
 		return discount;
 	}
-
 	public void setDiscount(int discount) {
 		this.discount = discount;
 	}
-
 	public String getCategory() {
 		return category;
 	}
-
 	public void setCategory(String category) {
 		this.category = category;
 	}
-
 	public String getImage() {
 		return image;
 	}
-
 	public void setImage(String image) {
 		this.image = image;
 	}
-
-	public String getDesc() {
-		return desc;
+	public String getDescription() {
+		return description;
 	}
-
-	public void setDesc(String desc) {
-		this.desc = desc;
+	public void setDescription(String description) {
+		this.description = description;
 	}
-
 	public int getTotalPurchased() {
 		return totalPurchased;
 	}
-
 	public void setTotalPurchased(int totalPurchased) {
 		this.totalPurchased = totalPurchased;
 	}
